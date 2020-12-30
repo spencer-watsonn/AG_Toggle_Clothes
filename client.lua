46201a83980a92de3cf2982c816636c28aa296f3
spencer-watsonn <75942065+spencer-watsonn@users.noreply.github.com>
2020-12-30 17:53:16 +0000

    Revamped script, and added hat option

--- client.lua
+++ client.lua
@@ -1,93 +1,52 @@
---[[
-	Know Bugs/Issues:
-		- When player ped changed, script no longer works
-]]
+-- toggleMask
+local toggleMask = 'mask' -- Defines the command for toggleMask
+local drawableMask
+local textureMask
 
-ESX = nil
-mask = true
-ped = GetPlayerPed(-1)
-local toggleMask = 244 -- Defines "244" to variable toggleMask (This will be the keybind to toggle the mask on/off - Current Keybind: [M]) 
+-- toggleHat
+local toggleHat = 'hat' -- Defines the command for toggleHat
+local drawableHat
+local textureHat
 
---===================================================================
+Citizen.CreateThread(function()
+	TriggerEvent('chat:addSuggestion', "/" .. toggleMask, 'Toggles mask On/Off')
+	TriggerEvent('chat:addSuggestion', "/" .. toggleHat, 'Toggles hat On/Off')
+end)
 
---[[
-	In this section, all the current clothing items and textures are defined to variables.
-	Said variables are then used later on in the code (L56-66 & L71).
-]]
+RegisterCommand(toggleMask, function()
+	local ped = GetPlayerPed(-1)
 
-drawableFace = GetPedDrawableVariation(ped, 0)
-textureFace = GetPedTextureVariation(ped, 0)
+	-- Does player have a mask
+	-- If yes, save mask and switch to no mask 
+	-- If no, switch to saved mask and reset vars
+	if GetPedDrawableVariation(ped, 1) ~= 0 then
+		-- Saves current mask variation
+		drawableMask = GetPedDrawableVariation(ped, 1)
+		textureMask = GetPedTextureVariation(ped, 1)
+		
+		SetPedComponentVariation(ped, 1, 0, -1, 1) -- Sets mask to no mask
+	else
+		SetPedComponentVariation(ped, 1, drawableMask, textureMask, 1) -- Sets mask to saved mask
+		drawableMask = nil
+		textureMask = nil
+	end
+end)
 
-drawableMask = GetPedDrawableVariation(ped, 1)
-textureMask = GetPedTextureVariation(ped, 1)
+RegisterCommand(toggleHat, function()
+	local ped = GetPlayerPed(-1)
 
-drawableHair = GetPedDrawableVariation(ped, 2)
-textureHair = GetPedTextureVariation(ped, 2)
-
-drawableTorso = GetPedDrawableVariation(ped, 3)
-textureTorso = GetPedTextureVariation(ped, 3)
-
-drawableLeg = GetPedDrawableVariation(ped, 4)
-textureLeg = GetPedTextureVariation(ped, 4)
-
-drawableBag = GetPedDrawableVariation(ped, 5)
-textureBag = GetPedTextureVariation(ped, 5)
-
-drawableShoes = GetPedDrawableVariation(ped, 6)
-textureShoes = GetPedTextureVariation(ped, 6)
-
-drawableAccessory = GetPedDrawableVariation(ped, 7)
-textureAccessory = GetPedTextureVariation(ped, 7)
-
-drawableUndershirt = GetPedDrawableVariation(ped, 8)
-textureUndershirt = GetPedTextureVariation(ped, 8)
-
-drawableKevlar = GetPedDrawableVariation(ped, 9)
-textureKevlar = GetPedTextureVariation(ped, 9)
-
-drawableBadge = GetPedDrawableVariation(ped, 10)
-textureBadge = GetPedTextureVariation(ped, 10)
-
-drawableTorso2 = GetPedDrawableVariation(ped, 11)
-textureTorso2 = GetPedTextureVariation(ped, 11)
-
---===================================================================
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-Citizen.CreateThread(function()    
-	while true do
-		Citizen.Wait(0)
-		if IsControlJustPressed(1, toggleMask) then -- Checks for a keypress
-
-			--===================================================================
-			
-			--[[
-				In this section, the players clothing is set the it's original excluding the mask.
-				The mask cannot be changed here due to it being the component we are toggling.
-			]]
-
-			SetPedComponentVariation(ped, 0, drawableFace, textureFace, 1)
-			SetPedComponentVariation(ped, 2, drawableHair, textureHair, 1)
-			SetPedComponentVariation(ped, 3, drawableTorso, textureTorso, 1)
-			SetPedComponentVariation(ped, 4, drawableLeg, textureLeg, 1)
-			SetPedComponentVariation(ped, 5, drawableBag, textureBag, 1)
-			SetPedComponentVariation(ped, 6, drawableShoes, textureShoes, 1)
-			SetPedComponentVariation(ped, 7, drawableAccessory, textureAccessory, 1)
-			SetPedComponentVariation(ped, 8, drawableUndershirt, textureUndershirt, 1)
-			SetPedComponentVariation(ped, 9, drawableKevlar, textureKevlar, 1)
-			SetPedComponentVariation(ped, 10, drawableBadge, textureBadge, 1)
-			SetPedComponentVariation(ped, 11, drawableTorso2, textureTorso2, 1)
-			
-			--===================================================================
-
-			if mask then
-				SetPedComponentVariation(ped, 1, 0, 0, 1) -- Sets mask to no mask
-				mask = false
-			else 
-				SetPedComponentVariation(ped, 1, drawableMask, textureMask, 1) -- Sets mask to original mask
-				mask = true
-			end
-		end		
-	end	
+	-- Does player have a hat
+	-- If yes, save hat and switch to no hat
+	-- If no, switch to saved hat and reset vars
+	if GetPedPropIndex(ped, 0) ~= -1 then
+		-- Saves current hat variation
+		drawableHat = GetPedPropIndex(ped, 0)
+		textureHat = GetPedPropTextureIndex(ped, 0)
+		
+		ClearPedProp(ped, 0) -- Sets hat to no hat
+	else
+		SetPedPropIndex(ped, 0, drawableHat, textureHat, false) -- Sets hat to saved hat
+		drawableHat = nil
+		textureHat = nil
+	end
 end)
